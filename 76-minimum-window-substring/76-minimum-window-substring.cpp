aff5a23144c662ee3046b0621cc68835aa25481a
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-02-26 09:43:42 +0530

    Time: 68 ms (19.81%), Space: 7.7 MB (67.35%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        int n=s.size();
+        int m=t.size();
+        string res="";
+        
+        if(n<m)
+            return res;
+        
+         map<char,int>patternHash,stringHash;
+        for(auto it:t)
+            patternHash[it]++;
+        
+        
+        int i=0;
+        int j=0;
+        int count=0;
+        int len=INT_MAX;
+        int startIndex=-1;
+        
+        while(j<n)
+        {
+            stringHash[s[j]]++;
+            
+            if(stringHash[s[j]]<=patternHash[s[j]])
+            {
+                count++;
+            }
+            if(count==m)
+            {
+                
+              //  cout<<i<<" "<<j<<endl;
+                
+                while(stringHash[s[i]]>patternHash[s[i]] or 
+                     patternHash[s[i]]==0)
+                {
+                    
+                    if(stringHash[s[i]]>patternHash[s[i]])
+                    stringHash[s[i]]--;
+                    
+                    i++;
+                    
+                }
+                //cout<<i<<endl;
+                
+                int found=j-i+1;
+                if(len>found)
+                {
+                    len=found;
+                    startIndex=i;
+                }
+               
+            }
+            j++;
+        }
+        if(startIndex==-1)
+            return "";
+        // cout<<startIndex<<" "<<len<<endl;
+        return s.substr(startIndex,len);
+        
+    }
+};
