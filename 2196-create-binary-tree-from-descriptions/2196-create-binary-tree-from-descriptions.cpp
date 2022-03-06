2e809ebfcb285229ce79130635cb89040fc027cf
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-03-06 11:04:57 +0530

    Time: 1120 ms (7.69%), Space: 293.3 MB (7.69%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        map<int,TreeNode *>ma;
+        map<int,int>indegree;
+        
+        for(auto i:descriptions)
+        {
+            int parent=i[0];
+            int child=i[1];
+            indegree[child]++;
+            if(ma.count(parent)==0)
+                ma[parent]=new TreeNode(parent);
+            if(ma.count(child)==0)
+                ma[child]=new TreeNode(child);
+            
+            if(i[2]==1)
+            {
+                ma[parent]->left=ma[child];
+            }
+            else
+            {
+                ma[parent]->right=ma[child];
+            }
+        }
+        
+        for(auto i:ma)
+        {
+            if(indegree[i.first]==0)
+                return i.second;
+        }
+        return NULL;
+        
+    }
+};
