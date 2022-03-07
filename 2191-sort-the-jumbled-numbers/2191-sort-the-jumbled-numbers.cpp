97e20856cb7037b19591638f8d35076adc4c7b1b
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-03-07 08:56:36 +0530

    Time: 1120 ms (14.29%), Space: 83.9 MB (28.57%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    map<int,int>ma;
+private:
+    int change(int x)
+    {
+        string res=to_string(x);
+        string fres="";
+        for(auto i:res)
+        {
+            fres+=to_string(ma[i-'0']);
+        }
+        return stoi(fres);
+    }
+public:
+    
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        vector<int>ans;
+        vector<pair<int,int>>temp;
+        
+        for(int i=0;i<=9;i++)
+            ma[i]=mapping[i];
+       
+        for(int i=0;i<nums.size();i++)
+        {
+            int res=change(nums[i]);
+            temp.push_back({res,i});
+        }
+        sort(temp.begin(),temp.end(),[](pair<int,int>p1,pair<int,int>p2){
+           return p1.first<p2.first; 
+        });
+        
+        for(auto i:temp)
+            ans.push_back(nums[i.second]);
+        
+        return ans;
+    }
+};
