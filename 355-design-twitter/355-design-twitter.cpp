2684bdb6f50ec77968dc939b446f7f2cbcb8eb97
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-03-11 10:40:33 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (46.67%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.cpp
@@ -0,0 +1,63 @@
+class Twitter {
+public:
+    deque<pair<int,int>>dq,tmp;
+    map<int,vector<int>>ma;
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        dq.push_front({userId,tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int>ans;
+        tmp=dq;
+        int k=10;
+        auto x=ma[userId];
+        map<int,int>hash;
+        for(auto i:x){
+         //   cout<<userId<<" "<<i<<endl;
+            hash[i]=1;
+        }
+        while(1)
+        {
+            if(k==0)
+                break;
+            if(tmp.empty())
+                break;
+            auto x=tmp.front();
+            if(x.first==userId or hash[x.first]){
+                ans.push_back(x.second);
+                k--;
+            }
+            tmp.pop_front();
+        
+        }
+       
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        ma[followerId].push_back(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        
+        vector<int>followers=ma[followerId];
+        auto it=find(followers.begin(),followers.end(),followeeId);
+        if(it==followers.end())
+            return;
+        followers.erase(it);
+        ma[followerId]=followers;
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
