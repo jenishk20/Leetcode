c431868e6bf2ca3496fb982990504f8bdd934968
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-03-15 22:07:50 +0530

    Time: 32 ms (53.31%), Space: 12.6 MB (14.23%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         stack<pair<char,int>>se;
-        bool be=false;
-        
+      
         int n=s.size();
-        string res="";
         vector<int>idx;
         int i;
         for(i=0;i<n;i++)
@@ -13,25 +11,24 @@ public:
             if(s[i]==')')
             {
                 if(se.empty()){
-                    res+=s[i];
+                   
                     idx.push_back(i);
                     continue;
                 }
                 else
                 {
-                    res+=s[i];
+                   
                     se.pop();
                     
                 }
             }
             else if(s[i]=='(')
             {
-                res+=s[i];
+                
                 
                 se.push({s[i],i});
             }
-            else
-                res+=s[i];
+           
         }
         
         while(!se.empty())
@@ -48,15 +45,15 @@ public:
           for(auto i:idx)
             cout<<i<<" ";
         cout<<endl;
-        for(i=0;i<res.size();i++)
+        for(i=0;i<s.size();i++)
         {
             if(k<idx.size() and i==idx[k])
             {
                 k++;
                 continue;
             }
-            cout<<res[i]<<" ";
-            fres+=res[i];
+         
+            fres+=s[i];
         }
         
         return fres;
