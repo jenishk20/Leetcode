fde175a19ffbdccd2bd6a084cb572957e2cb015f
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-03-25 08:35:38 +0530

    Time: 12 ms (23.24%), Space: 10.2 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    int dp[101][101][101];
-    int recur(int idx,int a,int b,vector<vector<int>>&costs)
-    {
-        if(idx>=costs.size())
-            return 0;
-        int op1=1e9,op2=1e9;
-        
-        if(dp[idx][a][b]!=-1)
-            return dp[idx][a][b];
-        if(a)
-        {
-            op1=costs[idx][0]+recur(idx+1,a-1,b,costs);
-            
-        }
-        if(b)
-        {
-            op2=costs[idx][1]+recur(idx+1,a,b-1,costs);
-        }
-        return dp[idx][a][b]=min(op1,op2);
-    }
+   
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
+        sort(costs.begin(),costs.end(),[](vector<int>a,vector<int>b){
+           return a[0]-b[0]<a[1]-b[1];
+        });
         
+        int res=0;
         int n=costs.size();
-        int req=n/2;
-        int req1=n/2;
-        memset(dp,-1,sizeof dp);
-        return recur(0,req,req1,costs);
+        for(int i=0;i<n/2;i++)
+        {
+            res+=(costs[i][0])+(costs[n-i-1][1]);
+        }
+        return res;
     }
 };
