5bdec88dc208987271e52da5063ed0058a126d54
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-04-27 08:55:17 +0530

    Time: 2098 ms (5.03%), Space: 106.1 MB (28.40%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,80 @@
+class Node
+{
+    public:
+    int u,v,wt;
+    Node(int _u,int _v,int _wt)
+    {
+        u=_u;
+        v=_v;
+        wt=_wt;
+    }
+};
+class Solution {
+public:
+    static bool comp(Node o1,Node o2)
+    {
+        return o1.wt<o2.wt;
+    }
+    int findPar(int u,vector<int>&parent)
+    {
+        if(u==parent[u])
+            return u;
+        return parent[u]=findPar(parent[u],parent);
+    }
+    void merge(int pu,int pv,vector<int>&parent)
+    {
+        parent[pu]=pv;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n=points.size();
+        int i,j,k;
+        vector<Node>ve;
+        map<int,pair<int,int>>ma;
+        for(i=0;i<n;i++)
+        {
+            ma[i]={points[i][0],points[i][1]};
+            
+            for(j=0;j<n;j++)
+            {
+                if(i==j)
+                    continue;
+                int cost=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                
+               
+                ve.push_back(Node(i,j,cost));
+            }
+        }
+        
+        
+        sort(ve.begin(),ve.end(),comp);
+        // for(auto i:ve)
+        // {
+        //     cout<<i.u<<" "<<i.v<<" "<<i.wt<<endl;
+        // }
+        int cost=0;
+        vector<int>parent(n+1);
+        for(i=0;i<=n;i++)
+            parent[i]=i;
+        
+        
+        for(i=0;i<ve.size();i++)
+        {
+            int u=ve[i].u;
+            int v=ve[i].v;
+            int wt=ve[i].wt;
+            
+            int pu=findPar(u,parent);
+            int pv=findPar(v,parent);
+            if(pu!=pv)
+            {
+                merge(pu,pv,parent);
+                cost+=wt;
+            }
+            
+                
+        }
+        
+        return cost;
+    }
+};
