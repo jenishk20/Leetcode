d08b7cf108746ae1955492f409b19ac912be2182
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-05-01 10:36:55 +0530

    Time: 1752 ms (33.33%), Space: 230.1 MB (33.33%) - LeetHub

---
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) {
+        int n=nums.size();
+        set<vector<int>>ans;
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            vector<int>temp;
+            int ct=0;
+            for(j=i;j<n;j++)
+            {
+                temp.push_back(nums[j]);
+                
+                if(nums[j]%p==0)
+                    ct++;
+                if(ct>k)
+                    break;
+                ans.insert(temp);
+            }
+            
+        }
+        
+        return ans.size();
+    }
+};
