370aea64c7c9c93a447b902a2d5a20f2124a77ea
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-05-03 17:43:52 +0530

    Time: 48 ms (38.37%), Space: 26.5 MB (68.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -7,21 +7,22 @@ public:
         int st=-1;
         int en=-1;
         int maxTillNow=INT_MIN;
+        
         for(i=0;i<n;i++)
         {
             if(nums[i]<maxTillNow)
             {
-               if(st==-1)
-                    st=i-1;
+                if(st==-1)
+                    st=i;
                 while(st-1>=0 and nums[st-1]>nums[i])
+                {
                     st--;
+                }
                 en=i+1;
             }
             else
             {
-                
-                 maxTillNow=nums[i];
-               // cout<<st<<" "<<en<<endl;
+                maxTillNow=nums[i];
             }
         }
   
