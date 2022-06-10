edab4a81f996c297f4a570f842e5997a313d8ce0
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-06-10 09:57:17 +0530

    Time: 198 ms (19.58%), Space: 69.5 MB (7.75%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& A, int bricks, int ladders) {
-            multiset<int> s;
-        for (int i = 0; i < A.size() - 1; i++) {
-            int d = A[i + 1] - A[i];
-            if (d > 0)
-                s.insert(A[i + 1] - A[i]);
-            if (s.size() > ladders) {
-                bricks -= *s.begin();
-                s.erase(s.begin());
-                if (bricks < 0) return i;
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        
+       multiset<int>se;
+        int i;
+        int n=heights.size();
+        for(i=0;i<n-1;i++)
+        {
+            int diff=heights[i+1]-heights[i];
+            if(diff>0)
+                se.insert(heights[i+1]-heights[i]);
+            
+            if(se.size()>ladders)
+            {
+                bricks-=(*se.begin());
+                se.erase(se.begin());
+                
+                if(bricks<0)
+                    return i;
             }
         }
-        return A.size() - 1;
+        
+        return n-1;
     }
 };
