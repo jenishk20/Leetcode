18c13001fcae8494c72168e0c2b4ed1414110990
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-08-22 10:51:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -5,18 +5,7 @@ public:
         
         if(n<=0)
             return false;
-        int cnt=0;
-        bool flag=false;
-        for(int i=0;i<=31;i++)
-        {
-            if(n&(1<<i))
-            {
-                if(i%2==0)
-                    flag=1;
-                cnt++;
-            }
-            
-        }
-        return flag and cnt==1;
+        
+        return (n&(n-1))==0  and (n&(0xaaaaaaaa))==0;
     }
 };
