b75694dca5b6ea59fb7df280cf53433eef491971
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-08-25 08:09:45 +0530

    Time: 31 ms (41.50%), Space: 9.1 MB (6.70%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        map<int,int>req,have;
-        for(auto i:ransomNote)
-            req[i]++;
+        
+        map<char,int>have,req;
         for(auto i:magazine)
             have[i]++;
+        
+        for(auto i:ransomNote)
+            req[i]++;
         
         for(auto i:req)
         {
             if(i.second>have[i.first])
+            {
                 return false;
+            }
         }
         return true;
     }
