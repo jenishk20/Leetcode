744abe78787d79af8901af794237622f44d86eac
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-10-18 08:46:26 +0530

    Time: 22 ms (33.89%), Space: 6.6 MB (77.40%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if (n == 0) return "";
-    string res = "1";
-    while (--n) {
-        string cur = "";
-        for (int i = 0; i < res.size(); i++) {
-            int count = 1;
-             while ((i + 1 < res.size()) && (res[i] == res[i + 1])){
-                count++;    
-                i++;
+        
+        if(n==0) return "";
+        
+        if(n==1)
+            return "1";
+        
+        int i;
+        string res="1";
+        while(--n)
+        {
+            string curr="";
+            for(i=0;i<res.size();i++)
+            {
+                int cnt=1;
+                char ch=res[i];
+                while(i+1<res.size() and res[i+1]==ch)
+                {
+                    i++;
+                    cnt++;
+                }
+                curr+=(to_string(cnt))+res[i];
+                
             }
-            cur += to_string(count) + res[i];
+           // cout<<curr<<endl;
+            res=curr;
         }
-        res = cur;
+        return res;
     }
-    return res;
-    }
+    
 };
