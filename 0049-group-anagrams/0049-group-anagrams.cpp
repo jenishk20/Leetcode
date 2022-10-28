c3de685371e71afb27f34d1946075280828c360b
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-10-28 09:41:32 +0530

    Time: 96 ms (25.83%), Space: 20.9 MB (34.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,67 +3,20 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
         
-        vector<string>temp;
-        map<string,vector<int>>ma;
-        for(auto i:strs)
+       
+        map<string,vector<string>>ma;
+     
+        for(int i=0;i<strs.size();i++)
         {
-            auto tt=i;
-            sort(tt.begin(),tt.end());
-            temp.push_back(tt);
-        }
-        for(int i=0;i<temp.size();i++)
-        {
-            ma[temp[i]].push_back(i);
+            string t=strs[i];
+            sort(t.begin(),t.end());
+            ma[t].push_back(strs[i]);
         }
         vector<vector<string>>ans;
         for(auto it:ma)
         {
-            vector<string>res;
-            for(auto it2:it.second)
-            {
-                res.push_back(strs[it2]);
-            }
-            ans.push_back(res);
+            ans.push_back(it.second);
         }
         return ans;
-//         vector<map<char,int>>ve;
-//         for(auto i:strs)
-//         {
-//             map<char,int>temp;
-//             for(auto it:i)
-//             {
-//                 temp[it]++;
-//             }
-//             ve.push_back(temp);
-//         }
-//         map<map<char,int>,int>ma;
-//         int st=0;
-//         for(auto i:ve){
-            
-//             if(ma.find(i)==ma.end())
-//             {
-                
-//             }
-//             ma[i]++;
-//         }
-        
-//         for(auto it:ma)
-//         {
-//             for(auto it1:it.first)
-//             {
-//                     cout<<it1.first<<" "<<it1.second<<endl;
-//             }
-//             cout<<it.second<<endl;
-//         }
-//         for(auto i:strs)
-//         {
-//             map<char,int>temp;
-//             for(auto it:i)
-//             {
-//                 temp[it]++;
-//             }
-//             if(ma.find())
-//         }
-//         return {};
     }
 };
