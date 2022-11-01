e6c4984ac7a656460cd15995b32b3acb53d3c93e
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2022-11-01 10:44:05 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -10,97 +10,47 @@ using namespace std;
 
 class Solution{   
 public:
-int countElementsLessThanMid(vector<int>&v, int x)
-
+int func(vector<int>&v,int x)
 {
-
     int lo=0, hi=v.size()-1;
-
     int cnt=0;
-
     while(lo<=hi)  
-
     {
 
         int mid=lo+(hi-lo)/2; 
-
         if(v[mid] < x){        // Checking for next highest element
-
-            cnt = (mid+1); // 0th-based indexing
-
+            cnt = (mid+1); // 0th-based indexing    
             lo=mid+1;
-
-        }   
-
-        else
-
-            hi=mid-1;
-
-    }
-
-    return cnt;
-
-}
-
-
-    int median(vector<vector<int>> &A, int R, int C){
-        // code here    
-        
-      int n=A.size(),m=A[0].size();
-
-    // int lo=1;
-
-    // int hi=1e9;
-
-    
-
-    // find min or max element in a matrix using first or last elements of each row respectively.
-
-    int lo = INT_MAX, hi = INT_MIN;
-
-    for(int i=0;i<n;i++){
-
-        lo = min(lo, A[i][0]);
-
-        hi = max(hi, A[i][m-1]);
-
-    }
-
-    
-
-    int ans;
-
-    while(lo<=hi)
-
-    {
-
-        int mid=lo+(hi-lo)/2;
-
-        int count=0;
-
-        // Every single row is sorted SO make use of that via Binary search
-
-        for(int i=0;i<n;i++)
-
-            count += countElementsLessThanMid(A[i], mid);
-
             
-        //cout<<count<<endl;
-        if(count<=(n*m/2)){
-
-            ans=mid;
-
-            lo=mid+1;
-
-        }
-
+        }   
         else
-
             hi=mid-1;
 
     }
-
-    return ans;      
+    return cnt;
+}
+    int median(vector<vector<int>> &matrix, int R, int C){
+        // code here   
+        int low=1;
+        int high=1e9;
+        int medPos=(R*C)/2;
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int count=0;
+            for(int i=0;i<R;i++)
+            {
+                count+=func(matrix[i],mid);
+                //cout<<count<<endl;
+            }
+            
+            
+            if(count<=medPos) {ans=mid;low=mid+1;}
+            else high=mid-1;
+            
+        }
+        return ans;
     }
 };
 
